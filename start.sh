19af0316f26fb0625fc4922e796d30e9f1370451
Roger Filomeno <rpfilomeno@gmail.com>
2022-02-22 20:09:04 +0000

    added tests and scanning

--- start.sh
+++ start.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
-cd ./test-app
-composer update
+
+echo "==> Starting installation of custom package local/bear-claw"
+cd /workspace/packages/cartoon
+composer install
+composer dump-autoload
+
+
+echo "==> Running unit tests for local/bear-claw"
+./vendor/bin/phpstan analyse -l 5 src tests
+
+echo "==> Starting intallation of Laravel test-app"
+cd /workspace/test-app
+composer install
+php artisan clear-compiled 
+composer dump-autoload
+php artisan optimize
+
+echo "==> Setting database of Laravel test-app"
 php artisan db:create
 php artisan migrate
 php artisan db:seed --class=UserSeeder
+
+echo "==> Starting wewbserver of Laravel test-app"
 php artisan serve
