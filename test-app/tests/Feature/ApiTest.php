5b1a81907b2d90244fbdf6c52a2de860face40f7
Roger Filomeno <rpfilomeno@gmail.com>
2022-02-23 20:10:56 +0000

    added missing test units

--- test-app/tests/Feature/ApiTest.php
+++ test-app/tests/Feature/ApiTest.php
@@ -2,19 +2,38 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ApiTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function test_test()
-    {
-        $response = $this->get('/check');
-        $response->assertStatus(200);
+
+   
+    public function test_CanCallCheckApi() : void {
+        $headers = [
+            'HTTP_Authorization' => 'Basic '. base64_encode('demo:pwd1234')
+        ];
+        
+        $this->get('/api/check',$headers)
+            ->assertStatus(200)
+            ->dump();
     }
+
+    public function test_CanCallTestApi() : void {
+        $headers = [
+            'HTTP_Authorization' => 'Basic '. base64_encode('demo:pwd1234'),
+            'CONTENT_TYPE' => 'application/json',
+            'HTTP_ACCEPT' => 'application/json'
+        ];
+
+        $payload = json_decode('{ "purchase_order_ids": ["2344", "2345", "2346"] }',true);
+        $expected = json_decode('{"result":[{"product_type_id":1,"total":41.5},{"product_type_id":2,"total":13.8},{"product_type_id":3,"total":25}]}',true);
+
+        $this->json('POST','/api/test', $payload, $headers)
+            ->assertJson($expected)
+            ->assertStatus(200)
+            ->dump();
+
+    }
+
+
 }
